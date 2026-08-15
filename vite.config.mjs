a54c22628b1217f71fbc1c5c3404821e29ba88d1
panderam1107 <panderam1107@gmail.com>
2026-08-15 20:44:57 +0530

    Fix comment formatting in vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+//t https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: './', // 👈 this fixes the blank page on deployment
